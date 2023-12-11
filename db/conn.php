4beb803a0288b952228db493e051221b347bb4c9
Lenice1 <lenice.brissett@gmail.com>
2023-12-11 14:19:16 -0500

    update

---
+++ db/conn.php
@@ -0,0 +1,18 @@
+<?php 
+    $host = '127.1.0.1';
+    $db = 'attendance_db';
+    $user = 'root';
+    $pass = '';
+    $charset = 'uft8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        echo 'Hello Database';
+    } catch(PDOException $e) {
+        //throw new PDOException($e->getMessage());
+
+    }
+
+?>
