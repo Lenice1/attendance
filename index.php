caa35c07720955a46d7047b60c16734d4924a8c6
Lenice1 <lenice.brissett@gmail.com>
2023-12-11 13:51:52 -0500

    update

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
         $title = 'Index';
        require_once 'includes/header.php';
        ?>
-    
-        <h1 class="text-center"> Registration for IT Conference </h1>
-
-        <!-- 
+            <!-- 
             - First Name
             - Last Name
             - DOB (Date Picker)
@@ -13,22 +10,26 @@
             - Email Address
             - Contact Number
         -->
-<form>
+
+<h1 class="text-center"> Registration for IT Conference </h1>
+
+
+<form method="post" action="success.php">
         <div class="form-group">
             <label for="firstname" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="firstname">
+            <input type="text" class="form-control" id="firstname" name="firstname">
         </div>
-        <div class="form-group">
+        <div class="form-group" >
             <label for="lastname" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="lastname">
+            <input type="text" class="form-control" id="lastname" name="lastname">
         </div>
         <div class="form-group">
             <label for="dob" class="form-label">Date of Birth</label>
-            <input type="text" class="form-control" id="dob" >
+            <input type="text" class="form-control" id="dob" name="dob">
         </div>
         <div class="form-group">
-            <label for="speaciality" >Area of Expertise</label>
-            <select class="form-control" id="speaciality">
+            <label for="specialty" >Area of Expertise</label>
+            <select class="form-control" id="specialty" name="specialty">
                 <option>Database Admin</option>
                 <option>Software Developer</option>
                 <option>Web Administrator</option>
@@ -37,19 +38,19 @@
         </div>
         <div class="form-group">
             <label for="email" class="form-label">Email Address </label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" name="email">
 
-            <small id="emailHelp" class="form-text text-muted"> We'll never share your Email with anyone else.</small>
+            <small id="emailHelp" class="form-text text-muted"> We'll never share your Email with anyone else</small>
 
         </div>
         <div class="form-group">
             <label for="phone" class="form-label">Contact Number</label>
-            <input type="text" class="form-control" id="phone">
+            <input type="text" class="form-control" id="phone" name="phone">
 
-            <small id="phoneHelp" class="form-text text-muted">We'll never share your contact with anyone else.</small>
+            <small id="phoneHelp" class="form-text text-muted">We'll never share your contact with anyone else</small>
         </div>
         
-        <button type="submit" class="btn btn-primary btn-block">Submit</button>
+        <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
 </form>
 <br>
 <br>
---
+++ success.php
@@ -0,0 +1,31 @@
+<?php 
+        $title = 'Success';
+       require_once 'includes/header.php';
+       ?>
+    
+        <h1 class="text-center text-success"> You Have Been Registered </h1>
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">
+        <?php echo $_POST['firstname'] . ' ' . $_POST['lastname'];?></h5>
+<h6 class="card-subtitle mb-2 text-body-secondary">
+    <?php echo $_POST['specialty'];?>
+</h6>
+    <p class="card-text">Date of Birth: 
+        <?php echo $_POST['dob'];?>
+    </p>
+    <p class="card-text">Email Address: 
+        <?php echo $_POST['email'];?>
+    </p>
+    <p class="card-text">Phone Number: 
+        <?php echo $_POST['phone'];?>
+    </p>
+  </div>
+</div>
+      
+<br>
+<br>
+<br>
+<br>
+    <?php require_once 'includes/footer.php'; ?>
