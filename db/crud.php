e694890f4a2ca59e1a3715d95170fda3ced73b59
Lenice1 <lenice.brissett@gmail.com>
2023-12-16 23:35:55 -0500

    up

--- db/crud.php
+++ db/crud.php
@@ -1,30 +1,28 @@
 <?php 
     class crud{
-        // private database object\
+        
         private $db;
         
-        //constructor to initialize private variable to the database connection
         function __construct($conn){
             $this->db = $conn;
         }
         
-        // function to insert a new record into the attendee database
         public function insertAttendees($fname, $lname, $dob, $email,$contact,$specialty,$avatar_path){
             try {
-                // define sql statement to be executed
-                $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id,avatar_path) VALUES (:fname,:lname,:dob,:email,:contact,:specialty,:avatar_path)";
-                //prepare the sql statement for execution
+                
+                $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id) VALUES (:fname,:lname,:dob,:email,:contact,:specialty)";
+             
                 $stmt = $this->db->prepare($sql);
-                // bind all placeholders to the actual values
+           
                 $stmt->bindparam(':fname',$fname);
                 $stmt->bindparam(':lname',$lname);
                 $stmt->bindparam(':dob',$dob);
                 $stmt->bindparam(':email',$email);
                 $stmt->bindparam(':contact',$contact);
                 $stmt->bindparam(':specialty',$specialty);
-                $stmt->bindparam(':avatar_path',$avatar_path);
+            
 
-                // execute statement
+               
                 $stmt->execute();
                 return true;
         
@@ -36,7 +34,7 @@
 
         public function editAttendee($id,$fname, $lname, $dob, $email,$contact,$specialty){
            try{ 
-                $sql = "UPDATE `attendee` SET `firstname`=:fname,`lastname`=:lname,`dateofbirth`=:dob,`emailaddress`=:email,`contactnumber`=:contact,`specialty_id`=:specialty WHERE attendee_id = :id ";
+                $sql = "INSERT INTO attendee VALUES (:fname, :lname, :dob, :email, :contact, :speciality)";
                 $stmt = $this->db->prepare($sql);
                 // bind all placeholders to the actual values
                 $stmt->bindparam(':id',$id);
--- success.php
+++ success.php
@@ -27,28 +27,6 @@
     }
 ?>
     
-    <!-- This prints out values that were passed to the action page using method="get" -->
-    <!-- <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">
-                <?php //echo $_GET['firstname'] . ' ' . $_GET['lastname'];  ?>
-            </h5>
-            <h6 class="card-subtitle mb-2 text-muted">
-                <?php //echo $_GET['specialty'];  ?>    
-            </h6>
-            <p class="card-text">
-                Date Of Birth: <?php //echo $_GET['dob'];  ?>
-            </p>
-            <p class="card-text">
-                Email Adress: <?php //echo $_GET['email'];  ?>
-            </p>
-            <p class="card-text">
-                Contact Number: <?php //echo $_GET['phone'];  ?>
-            </p>
-    
-        </div>
-    </div> -->
-
     <!-- This prints out values that were passed to the action page using method="post" -->
     <img src="<?php echo $destination; ?>" class="rounded-circle" style="width: 20%; height: 20%" />
     <div class="card" style="width: 18rem;">
